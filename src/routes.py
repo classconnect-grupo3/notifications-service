bebb39245a82d63fb42eaf946582ef10854e149f
tinchomorilla <mmorilla@fi.uba.ar>
2025-06-07 00:07:56 -0300

    add FCM token router to user routes

--- src/routes.py
+++ src/routes.py
@@ -3,7 +3,7 @@ from fastapi import APIRouter
 from src.controller.notifications_preferences import (
     router as notifications_preferences_router,
 )
-
+from src.controller.user_fcm_token import router as fcm_token_router
 
 router = APIRouter()
 
@@ -12,4 +12,11 @@ router.include_router(
     notifications_preferences_router,
     prefix="/preferences",
     tags=["Notification Preferences"],
+)
+
+
+router.include_router(
+    fcm_token_router,
+    prefix="user",
+    tags=["FCM Token"],
 )
